1d6c36b7c74d02e302f75e0ab43aafb8612c5ebb
vinicius-everis <vinicius.sferreira@everis.nttdata.com>
2021-10-11 11:31:07 -0300

    Curso de Testes no Android - Parte 1

---
+++ app/src/main/java/br/com/alura/leilao/model/Lance.java
@@ -0,0 +1,26 @@
+package br.com.alura.leilao.model;
+
+import android.support.annotation.NonNull;
+
+import java.io.Serializable;
+
+public class Lance implements Serializable, Comparable {
+
+    private final Usuario usuario;
+    private final double valor;
+
+    public Lance(Usuario usuario, double valor) {
+        this.usuario = usuario;
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    @Override
+    public int compareTo(@NonNull Object o) {
+        Lance lance = (Lance) o;
+        return (int) (lance.getValor() - valor);
+    }
+}
